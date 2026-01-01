ceae1312e66656cbe827b0cbdd05127a579ec599
India Daniel <igracedaniel@gmail.com>
2026-01-01 16:17:43 -0500

    adjustable x axis resolution

--- question5-interactiveApp/textInputGraphingApp.py
+++ question5-interactiveApp/textInputGraphingApp.py
@@ -15,10 +15,16 @@ frame.geometry('500x800')
 headerLabel = tk.Label(frame, text="Line Grapher with Text Input")
 headerLabel.pack()
 
-inputLabel = tk.Label(frame, text="Text Input:")
+inputLabel = tk.Label(frame, text="Slope Value:")
 inputLabel.pack()
 slopeInput = tk.Entry(frame)
 slopeInput.pack()
+
+resolutionLabel = tk.Label(frame, text="Number of X Values:")
+resolutionLabel.pack()
+resolutionInput = tk.Entry(frame)
+resolutionInput.pack() 
+
 # empty mpl and tkinter canvases
 matplotfigure = Figure(figsize = (8,6), dpi=100)
 canvas = FigureCanvasTkAgg(matplotfigure, master=frame)
@@ -29,25 +35,26 @@ def generatePlot(canvas, figure):
         figure.clear()
     
     slope = slopeInput.get()
+    resolution = resolutionInput.get()
+    print('resolution:',resolution)
 
-    # x = np.linspace(0, 10, 100)
-    x = np.linspace(0, 10, 10)
+    if resolution:
+        resolution = int(resolution)
 
-    # y = lambda: slope * x
-    y = [0]*10
-    # print("\nX:",x)
+        x = np.linspace(0, 10, resolution)
+        y = [0]*resolution
 
-    for x_value in range(len(x)):
-        # print('\nx_value:',int(x_value))
-        # print('vnslkdvnsldvsldnv:',y[int(x_value)])
-        y[x_value] = slope * x_value
-    
-    print('\nx',x)
-    print("y",y)
-    
-    subplot = figure.add_subplot()
-    subplot.plot(x, y)
-    canvas.draw()
+        for x_value in range(len(x)):
+            y[x_value] = slope * x_value
+        
+        print('\nx',x)
+        print("y",y)
+        
+        subplot = figure.add_subplot()
+        subplot.plot(x, y)
+        canvas.draw()
+    else:
+        print('resolution required before graphing...')
 
 plottingButton = tk.Button(frame, text="Generate Plot", command=lambda:generatePlot(canvas, matplotfigure))
 plottingButton.pack()
