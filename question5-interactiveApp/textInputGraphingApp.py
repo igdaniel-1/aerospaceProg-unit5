402507afbfac5a88a56d83ce23c73129cc7073b0
India Daniel <igracedaniel@gmail.com>
2026-01-01 15:55:01 -0500

    line graph plotting app with free text input

--- question5-interactiveApp/textInputGraphingApp.py
+++ question5-interactiveApp/textInputGraphingApp.py
@@ -2,22 +2,23 @@ import tkinter as tk
 from tkinter import ttk
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy import array
 from matplotlib.figure import Figure 
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,  
 NavigationToolbar2Tk) 
 import csv
 
 frame = tk.Tk()
-frame.title("Sin Plotter")
+frame.title("Line Plotter")
 frame.geometry('500x800')
 
-headerLabel = tk.Label(frame, text="Sin Grapher with Custom Slider")
+headerLabel = tk.Label(frame, text="Line Grapher with Text Input")
 headerLabel.pack()
 
-# input slider
-amplitudeSlider = tk.Scale(frame, from_=0, to=20, tickinterval=1, orient=tk.HORIZONTAL,length=400)
-amplitudeSlider.set(1) #sets the default slider value
-amplitudeSlider.pack()
+inputLabel = tk.Label(frame, text="Text Input:")
+inputLabel.pack()
+slopeInput = tk.Entry(frame)
+slopeInput.pack()
 # empty mpl and tkinter canvases
 matplotfigure = Figure(figsize = (8,6), dpi=100)
 canvas = FigureCanvasTkAgg(matplotfigure, master=frame)
@@ -27,11 +28,23 @@ def generatePlot(canvas, figure):
     if figure:
         figure.clear()
     
-    amplitude = amplitudeSlider.get()
+    slope = slopeInput.get()
 
-    x = np.linspace(0, 10, 100)
-    y = amplitude * np.sin(x)
+    # x = np.linspace(0, 10, 100)
+    x = np.linspace(0, 10, 10)
 
+    # y = lambda: slope * x
+    y = [0]*10
+    # print("\nX:",x)
+
+    for x_value in range(len(x)):
+        # print('\nx_value:',int(x_value))
+        # print('vnslkdvnsldvsldnv:',y[int(x_value)])
+        y[x_value] = slope * x_value
+    
+    print('\nx',x)
+    print("y",y)
+    
     subplot = figure.add_subplot()
     subplot.plot(x, y)
     canvas.draw()
@@ -39,20 +52,20 @@ def generatePlot(canvas, figure):
 plottingButton = tk.Button(frame, text="Generate Plot", command=lambda:generatePlot(canvas, matplotfigure))
 plottingButton.pack()
 
-# download x,y values as see in grpah and adjusted from the slider
-def downloadGraph():
-    # amplitude = amplitudeSlider.get()
-    x = np.linspace(0,10,100)
-    with open('question5-interactiveApp/slopeData.csv','w',newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=['x','y'])
-        writer.writeheader()
+# # download x,y values as see in grpah and adjusted from the slider
+# def downloadGraph():
+#     # amplitude = amplitudeSlider.get()
+#     x = np.linspace(0,10,100)
+#     with open('question5-interactiveApp/slopeData.csv','w',newline='') as file:
+#         writer = csv.DictWriter(file, fieldnames=['x','y'])
+#         writer.writeheader()
 
-        for x_value in range(len(x)):
-            y = amplitude * np.sin(x[x_value])
-            writer.writerow({'x':x[x_value], 'y':y})
+#         for x_value in range(len(x)):
+#             y = amplitude * np.sin(x[x_value])
+#             writer.writerow({'x':x[x_value], 'y':y})
 
-downloadButton = tk.Button(frame, text="Download Data (.csv)", command=downloadGraph)
-downloadButton.pack()
+# downloadButton = tk.Button(frame, text="Download Data (.csv)", command=downloadGraph)
+# downloadButton.pack()
 
 canvas.get_tk_widget().pack()
 generatePlot(canvas, matplotfigure)
